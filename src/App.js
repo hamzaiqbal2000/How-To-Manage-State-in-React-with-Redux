ef19e477709badf6b817cdbabc1cdfc83a7d2db8
hamzaiqbal2000 <hiqbal.bscs18seecs@seecs.edu.pk>
2022-04-11 12:40:05 +0500

    Setting Up a Store

--- src/App.js
+++ src/App.js
@@ -4,8 +4,32 @@ import { useSelector } from "react-redux";
 function App() {
   const birds = useSelector((state) => state.birds);
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="wrapper">
+      <ul>
+        <h1>Bird List</h1>
+        <form action="">
+          <label htmlFor="">
+            <p>Add a bird</p>
+            <input type="text" />
+          </label>
+          <div>
+            <button type="submit">Add</button>
+          </div>
+        </form>
+        {birds.map((bird) => (
+          <li key={bird.name}>
+            <h3>{bird.name}</h3>
+            <div>
+              Views: {bird.views} <br />
+              <button>
+                <span role="img" aria-label="add">
+                  ➕
+                </span>
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
